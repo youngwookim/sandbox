868e2b60cbf608fd2520ff9762d91f5c331e8984
Youngwoo Kim <brandon.kim@nexr.com>
2014-05-26 09:59:44 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.r
 rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 
 yum install -y --enablerepo=rpmforge-extras git
-yum install -y snappy snappy-devel make 
+yum install -y snappy snappy-devel wget make rpm-build fuse-devel cmake fuse-libs redhat-rpm-config lzo-devel autoconf automake redhat-lsb nc
 
 wget http://apache.mirror.cdnetworks.com//ant/binaries/apache-ant-1.9.4-bin.tar.gz
 wget http://mirror.apache-kr.org/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
@@ -36,14 +36,22 @@ tar xvfz apache-maven-3.2.1-bin.tar.gz
 echo "export JAVA_HOME=/usr/java/default" >> /etc/profile.d/java.sh
 echo "export ANT_HOME=/home/vagrant/apache-ant-1.9.4" >> /home/vagrant/.bashrc
 echo "export MAVEN_HOME=/home/vagrant/apache-maven-3.2.1" >> /home/vagrant/.bashrc
-echo "export PATH=\$JAVA_HOME/bin:\$ANT_HOME/bin:\$MAVEN_HOME/bin:\$PATH" >> /home/vagrant/.bashrc
+
+# ProtocolBuffers 2.5
+wget http://download.opensuse.org/repositories/home:/mrdocs:/protobuf-rpm/CentOS_CentOS-6/home:mrdocs:protobuf-rpm.repo -O /etc/yum.repos.d/protobuf.repo
+yum install -y protobuf-devel
+
+# Gradle
+wget https://services.gradle.org/distributions/gradle-1.12-bin.zip
+unzip gradle-1.12-bin.zip
+echo "export GRADLE_HOME=/home/vagrant/gradle-1.12" >> /home/vagrant/.bashrc
+
+# PATH
+echo "export PATH=\$JAVA_HOME/bin:\$ANT_HOME/bin:\$MAVEN_HOME/bin:\$GRADLE_HOME/bin:\$PATH" >> /home/vagrant/.bashrc
 
 # MySQL
 #service mysqld start
-
 #mysqladmin -u root password 'mypassword'
-
-# MySQL user and databases
 #mysql -uroot -pmypassword -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'mypassword';"
 #mysql -uroot -pmypassword -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
 #mysql -uroot -pmypassword -e "CREATE USER 'root'@'%' IDENTIFIED BY 'mypassword';"
