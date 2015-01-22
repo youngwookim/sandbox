ea34f40ca95d0ee0be0df9ba07aa5acd2d217689
Youngwoo Kim <warwithin@gmail.com>
2015-01-22 16:01:23 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -25,27 +25,9 @@ rpm -Uvh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6
 # Git
 yum install -y --enablerepo=rpmforge-extras git
 
-yum install -y snappy snappy-devel wget make rpm-build fuse-devel cmake fuse-libs redhat-rpm-config lzo-devel autoconf automake redhat-lsb nc createrepo asciidoc python-devel libxml2-devel libxslt-devel cyrus-sasl-devel openldap-devel mysql-devel xmlto nodejs npm
+yum install -y java-1.7.0-openjdk-devel snappy snappy-devel wget make rpm-build fuse-devel cmake fuse-libs redhat-rpm-config lzo-devel autoconf automake redhat-lsb nc createrepo asciidoc python-devel libxml2-devel libxslt-devel cyrus-sasl-devel openldap-devel mysql-devel xmlto nodejs npm
 
 npm install -g brunch@1.7.17
-
-# JDK7
-wget --no-cookies \
---no-check-certificate \
---header "Cookie: oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/7u71-b14/jdk-7u71-linux-x64.rpm" \
--O /tmp/jdk-7-linux-x64.rpm
-
-rpm -Uvh /tmp/jdk-7-linux-x64.rpm
-
-alternatives --install /usr/bin/java java /usr/java/jdk1.7.0_60/jre/bin/java 20000
-alternatives --install /usr/bin/jar jar /usr/java/jdk1.7.0_60/bin/jar 20000
-alternatives --install /usr/bin/javac javac /usr/java/jdk1.7.0_60/bin/javac 20000
-alternatives --install /usr/bin/javaws javaws /usr/java/jdk1.7.0_60/jre/bin/javaws 20000
-alternatives --set java /usr/java/jdk1.7.0_60/jre/bin/java
-alternatives --set javaws /usr/java/jdk1.7.0_60/jre/bin/javaws
-alternatives --set javac /usr/java/jdk1.7.0_60/bin/javac
-alternatives --set jar /usr/java/jdk1.7.0_60/bin/jar
 
 wget http://apache.mirror.cdnetworks.com//ant/binaries/apache-ant-1.9.4-bin.tar.gz
 wget http://mirror.apache-kr.org/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz 
@@ -55,7 +37,7 @@ wget http://archive.apache.org/dist/forrest/0.9/apache-forrest-0.9.tar.gz
 tar xvfz apache-ant-1.9.4-bin.tar.gz --directory=/opt/
 tar xvfz apache-maven-3.2.5-bin.tar.gz --directory=/opt/
 tar xvfz apache-forrest-0.9.tar.gz --directory=/opt/
-echo "export JAVA_HOME=/usr/java/default" >> /etc/profile.d/java.sh
+echo "export JAVA_HOME=/usr/lib/jvm/java-openjdk" >> /etc/profile.d/java.sh
 echo "export ANT_HOME=/opt/apache-ant-1.9.4" >> /etc/profile.d/ant.sh
 echo "export MAVEN_HOME=/opt/apache-maven-3.2.5" >> /etc/profile.d/maven.sh
 echo "export FORREST_HOME=/opt/apache-forrest-0.9" >> /etc/profile.d/forrest.sh
