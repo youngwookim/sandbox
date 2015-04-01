80f02e4145472e17e43658c7f3921bb1ae092c13
Youngwoo Kim <brandon.kim@nexr.com>
2015-04-01 16:23:27 +0900

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,11 +1,15 @@
 # Disable SELINUX
 sed -i -e 's@SELINUX=enforcing@SELINUX=disabled@' /etc/selinux/config
 
+# Puppetlabs
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-fedora-20.noarch.rpm
 
 yum -y install git puppet
+
+# FIXME
 yum -y update vim-minimal
 
 git clone https://github.com/apache/bigtop.git
 cd bigtop/bigtop_toolchain
 puppet apply --debug --modulepath=/home/vagrant/bigtop -e "include bigtop_toolchain::installer"
+chown vagrant:vagrant -R /home/vagrant/bigtop
