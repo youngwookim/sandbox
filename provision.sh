065d7ec5218dca359ea87ddd585c155b11aadd45
YoungWoo Kim <ywkim@apache.org>
2015-04-22 16:44:23 +0900

    Add unzip

--- provision.sh
+++ provision.sh
@@ -1,15 +1,13 @@
 # Disable SELINUX
 sed -i -e 's@SELINUX=enforcing@SELINUX=disabled@' /etc/selinux/config
 
-# Puppetlabs
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-fedora-20.noarch.rpm
-
-yum -y install git puppet
-
-# FIXME
+yum -y install git puppet unzip
 yum -y update vim-minimal
 
 git clone https://github.com/apache/bigtop.git
-cd bigtop/bigtop_toolchain
-puppet apply --debug --modulepath=/home/vagrant/bigtop -e "include bigtop_toolchain::installer"
+
+cd bigtop/
+./gradlew toolchain
+
 chown vagrant:vagrant -R /home/vagrant/bigtop
+
